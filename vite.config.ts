949d6d8b773408ca7115eb82dbbfc6fc4a0643ac
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2026-01-15 21:56:17 +0530

    new route changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: "/demo-apartment-website",
+    port: 3000,
     hmr: {
       overlay: false,
     },
